a30b5848ac34eb5eceee6d33f4538a7704276890
Bielgomes <bielgomesdasilva@hotmail.com>
2025-04-02 18:37:15 -0300

    3d x-axis rotation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from pygame import QUIT, Clock, display, event, init, quit
+from math import cos, sin
+
+import numpy as np
+from pygame import QUIT, Clock, display, draw, event, init, quit
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -6,11 +9,22 @@ RED = (255, 0, 0)
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 is_running = True
+angle = 0
+
+X_OFFSET = WIDTH // 2
+Y_OFFSET = HEIGHT // 2
+SCALE = 100
 
 init()
 
 screen = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Simple Pygame 3D Projection")
+
+points = []
+points.append(np.matrix([-1, -1, 1]))
+points.append(np.matrix([-1, 1, 1]))
+points.append(np.matrix([1, -1, 1]))
+points.append(np.matrix([1, 1, 1]))
 
 clock = Clock()
 while is_running:
@@ -23,6 +37,20 @@ while is_running:
     for e in event.get():
         if e.type == QUIT:
             is_running = False
+
+    rotation_matrix_x = np.matrix(
+        [[1, 0, 0], [0, cos(angle), -sin(angle)], [0, sin(angle), cos(angle)]]
+    )
+    angle += 0.01
+
+    # Draw the points
+    for point in points:
+        rotated_point = rotation_matrix_x * point.reshape(3, 1)
+
+        x = int(rotated_point[0, 0] * SCALE) + X_OFFSET
+        y = int(rotated_point[1, 0] * SCALE) + Y_OFFSET
+
+        draw.circle(screen, RED, (x, y), 5)
 
     # update the full display Surface to the screen
     display.flip()
